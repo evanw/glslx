d33a29199dcb946738bb957c1ed6f27baa5e9a59
Evan Wallace <evan.exe@gmail.com>
2021-01-21 00:36:02 -0800

    add typescript type definitions

---
+++ npm/glslx.d.ts
@@ -0,0 +1,103 @@
+// This is for '#include'
+type FileAccess = (filePath: string, relativeTo: string) => (null | string | { name: string, contents: string });
+
+interface CompileArgs {
+  format?: 'json' | 'js' | 'c++' | 'skew' | 'rust'; // Default: 'json'
+  renaming?: 'all' | 'internal-only' | 'none'; // Default: 'all'
+  disableRewriting?: boolean; // Default: false
+  prettyPrint?: boolean; // Default: false
+  keepSymbols?: boolean; // Default: false
+  fileAccess?: FileAccess;
+}
+
+export function compile(input: string, args?: CompileArgs): {
+  log: string;
+  output: string | null;
+};
+
+interface CompileArgsIDE {
+  fileAccess?: FileAccess;
+}
+
+interface LineColumn {
+  line: number;
+  column: number;
+}
+
+interface Range {
+  source: string;
+  start: LineColumn;
+  end: LineColumn;
+}
+
+export function compileIDE(input: string, args?: CompileArgsIDE): {
+  diagnostics: {
+    kind: 'error' | 'warning';
+    text: string;
+    range: Range | null;
+  }[];
+
+  tooltipQuery(message: {
+    id: any;
+    source: string;
+    line: number;
+    column: number;
+    ignoreDiagnostics: boolean;
+  }): {
+    type: 'tooltip-query';
+    id: any;
+    source: string;
+    tooltip: string;
+    range: Range;
+    symbol: string;
+  };
+
+  definitionQuery(message: {
+    id: any;
+    source: string;
+    line: number;
+    column: number;
+  }): {
+    type: 'definition-query';
+    id: any;
+    source: string;
+    definition: Range;
+    range: Range;
+    symbol: string;
+  };
+
+  symbolsQuery(message: {
+    id: any;
+    source: string;
+  }): {
+    type: 'symbols-query';
+    id: any;
+    source: string;
+    symbols: {
+      name: string;
+      kind: 'variable' | 'function' | 'struct';
+      range: Range;
+    }[];
+  };
+
+  renameQuery(message: {
+    id: any;
+    source: string;
+    line: number;
+    column: number;
+  }): {
+    type: 'rename-query';
+    id: any;
+    source: string;
+    ranges: Range[];
+    symbol: string;
+  };
+};
+
+interface FormatArgs {
+  indent?: string; // Default: '  '
+  newline?: string; // Default: '\n'
+  trailingNewline?: 'preserve' | 'remove' | 'insert'; // Default: 'insert'
+}
+
+export function format(input: string, args?: FormatArgs): string;
